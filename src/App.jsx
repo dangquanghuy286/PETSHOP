46448cbe8283cd53a5d14d5a05b38651866e298c
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-03-03 00:18:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel_1 from './components/caroul';
+function App() {
+  return (
+    <>
+      <Carousel_1 />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/caroul/index.jsx
@@ -0,0 +1,21 @@
+import { Carousel } from "bootstrap";
+import Button from 'react-bootstrap/Button';
+function Carousel_1() {
+
+
+    return (
+        <>
+            <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="success">Success</Button>
+            <Button variant="warning">Warning</Button>
+            <Button variant="danger">Danger</Button>
+            <Button variant="info">Info</Button>
+            <Button variant="light">Light</Button>
+            <Button variant="dark">Dark</Button>
+            <Button variant="link">Link</Button>
+
+        </>
+    )
+}
+export default Carousel_1;
