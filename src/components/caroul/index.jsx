cc576b1f4685eadf31a9df9679a359e325ee09ae
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-03-03 21:12:55 +0700

    set up

--- src/components/caroul/index.jsx
+++ src/components/caroul/index.jsx
@@ -5,15 +5,7 @@ function Carousel_1() {
 
     return (
         <>
-            <Button variant="primary">Primary</Button>
-            <Button variant="secondary">Secondary</Button>
-            <Button variant="success">Success</Button>
-            <Button variant="warning">Warning</Button>
-            <Button variant="danger">Danger</Button>
-            <Button variant="info">Info</Button>
-            <Button variant="light">Light</Button>
-            <Button variant="dark">Dark</Button>
-            <Button variant="link">Link</Button>
+
 
         </>
     )
