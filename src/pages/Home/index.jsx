2b17defbc2cd85ca952759f0276b92773ac1bac1
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-03-04 22:33:00 +0700

    banner 2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,17 +4,27 @@ import "./Home.scss"
 function Home() {
     return (
         <>
-            <div className="banner">
-                <div className="banner__text">
-                    <p className="banner__sale">PET-SHOP GIẢM GIÁ 30%</p>
-                    <h1 className="banner__title">Chúng tôi quan tâm đến thú cưng của bạn!</h1>
+            <div className="first-banner">
+                <div className="first-banner__text">
+                    <p className="first-banner__sale">PET-SHOP GIẢM GIÁ 30%</p>
+                    <h1 className="first-banner__title">Chúng tôi quan tâm đến thú cưng của bạn!</h1>
                 </div>
             </div>
+
             <Facility />
             <Store_Category />
 
-
+            <div className="second-banner">
+                <div className="second-banner__text">
+                    <h5 className="second-banner__discount">"Giảm giá 30% Sản phẩm Hữu cơ"</h5>
+                    <h2 className="second-banner__title">
+                        Thức ăn ngon & lành mạnh! <br />
+                    </h2>
+                    <button className="second-banner__button">Mua Ngay</button>
+                </div>
+            </div>
         </>
+
     );
 }
 export default Home;
