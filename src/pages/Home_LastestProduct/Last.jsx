1fd67297898baf9374fca1ceadbf67c3e8cdddde
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-03-06 03:06:58 +0700

    done

---
+++ src/pages/Home_LastestProduct/Last.jsx
@@ -0,0 +1,47 @@
+
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, updateQuantity } from "../../actions/cart";
+
+function LatestPd(props) {
+    const { item } = props;
+    const cart = useSelector(state => state.cartReducer);
+    const ditch = useDispatch();
+
+    const handleAddToCart = () => {
+        if (cart.some(itemCart => itemCart.id === item.id)) {
+            ditch(updateQuantity(item.id))
+        } else {
+            ditch(addToCart(item.id, item))
+        }
+    }
+
+    return (
+        <>
+            <div className="latest-products__item" key={item.id}>
+                <div className="latest-products__image">
+                    <img src={item.image} alt={item.name} />
+                    <div className="latest-products__tag">{item.discount}</div>
+                    <div className="latest-products__icons">
+                        <i className="fa-solid fa-heart"></i>
+                        <i className="fa-solid fa-eye"></i>
+                    </div>
+                </div>
+                <div className="latest-products__details">
+                    <div className="latest-products__reviews">
+                        {[...Array(5)].map((_, i) => (
+                            <i key={i} className="fa-solid fa-star"></i>
+                        ))}
+                    </div>
+                    <span className="latest-products__sub-category">Số lượng: {item.category}</span>
+                    <h6 className="latest-products__name">{item.name}</h6>
+                    <div className="latest-products__price">
+                        <del>{item.oldPrice}đ</del>
+                        <span>{item.newPrice}đ</span>
+                    </div>
+                    <button onClick={handleAddToCart} className="latest-products__button">Thêm Vào Giỏ Hàng</button>
+                </div>
+            </div>
+        </>
+    )
+}
+export default LatestPd
--- src/pages/Home_LastestProduct/index.jsx
+++ src/pages/Home_LastestProduct/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import "./Latest.scss"
 import { getLatest } from "../../services/cartegoryService copy";
+import LatestPd from "./Last";
 function HomeLatest() {
     const [data, setData] = useState([]);
     useEffect(() => {
@@ -22,30 +23,7 @@ function HomeLatest() {
                     </div>
                     <div className="latest-products__list">
                         {data.map((item) => (
-                            <div className="latest-products__item" key={item.id}>
-                                <div className="latest-products__image">
-                                    <img src={item.image} alt={item.name} />
-                                    <div className="latest-products__tag">{item.discount}</div>
-                                    <div className="latest-products__icons">
-                                        <i className="fa-solid fa-heart"></i>
-                                        <i className="fa-solid fa-eye"></i>
-                                    </div>
-                                </div>
-                                <div className="latest-products__details">
-                                    <div className="latest-products__reviews">
-                                        {[...Array(5)].map((_, i) => (
-                                            <i key={i} className="fa-solid fa-star"></i>
-                                        ))}
-                                    </div>
-                                    <span className="latest-products__sub-category">Số lượng: {item.category}</span>
-                                    <h6 className="latest-products__name">{item.name}</h6>
-                                    <div className="latest-products__price">
-                                        <del>{item.oldPrice}đ</del>
-                                        <span>{item.newPrice}đ</span>
-                                    </div>
-                                    <button className="latest-products__button">Thêm Vào Giỏ Hàng</button>
-                                </div>
-                            </div>
+                            <LatestPd item={item} key={item.id} />
                         ))}
                     </div>
                 </div>
